3b197da6adb492ce303dfae736a673998cd90af1
ithalocassiano <50937794+ithalocassiano@users.noreply.github.com>
2020-07-29 20:49:31 -0400

    Form teste

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -10,7 +10,20 @@ function CadastroCategoria () {
                 <h1>Cadastro de Categoria</h1>
 
                 <br/>
+                <form>
+                    <label>
+                        NOME DA CATEGORIA: 
+                        <input
+                            type="text"
+                        />
+                    </label>
 
+                    <button>
+                        Cadastrar agora
+                    </button>
+                </form>
+
+                
                 <Link to="/">
                     Ir para home
                 </Link>
